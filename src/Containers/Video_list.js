d3b8bd36b69941ddf927accc6554ee7cbb9a4a2c
Florent Heyraud <35958489+florenthey@users.noreply.github.com>
2019-01-20 22:54:21 +0100

    Update Video_list.js

--- src/Containers/Video_list.js
+++ src/Containers/Video_list.js
@@ -2,9 +2,6 @@ import React from "react";
 import VideoListItem from "../Components/Video_list_item"; 
 
 
-//Child of App
-//One item movie's list and container of all composents
-//Parameter callback receives the child component Video_list_item props
 const VideoList = (props) => {
     const {movieList} = props;
 
